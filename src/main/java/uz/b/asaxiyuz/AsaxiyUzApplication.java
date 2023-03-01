e5f1826a7ec841483e87d5f6dd3133c1112c8cd9
Baxtiy2370 <abdurashidovbaxtiyor00@gmail.com>
2023-03-01 14:34:27 +0500

    created

---
+++ src/main/java/uz/b/asaxiyuz/AsaxiyUzApplication.java
@@ -0,0 +1,13 @@
+package uz.b.asaxiyuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaxiyUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsaxiyUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/b/asaxiyuz/AsaxiyUzApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.b.asaxiyuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaxiyUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
